2af6264d69627e1dbf4acb39bd26ece011eb8833
dicklant <dicklant@hotmail.com>
2023-11-19 00:23:13 +0100

    Init projet

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require("http");
+
+const host = 'localhost';
+const port = normalizePort(process.env.PORT || '3000');
+
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end(`My little server`);
+ };
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
