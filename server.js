469c137ecff1b5dd684a0b2d190c3ee9a95ed0ca
dicklant <dicklant@hotmail.com>
2023-11-19 00:36:55 +0100

    suppr localhost

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ const requestListener = function (req, res) {
  };
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
+server.listen(port, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
